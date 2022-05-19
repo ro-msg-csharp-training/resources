6ce49f247cf408dc0e79189516a163fc2243021e
ovibud <105703077+ovibud@users.noreply.github.com>
2022-05-19 13:10:42 +0300

    Obud (#7)
    * merged remote
    
    * ___
    
    * ___
    
    * __
    
    * Delete Sources/Chapter1/Startup_Chapter/OnlineOrder/obj directory
    
    * __
    
    * ___
    
    * Added rest of model classes
    
    * Commented some unnecessary bindings in product class
    
    * added solutions for the other two apps in chapter one
    
    * added gitattributes
    
    * added solutions in chapter one for db access and repos
    
    Co-authored-by: Ovidiu Budisan <Ovidiu.Budisan@msg.group>

---
+++ Sources/Chapter1/RepoDecoupling/OnlineOrder/Model/Product.cs
@@ -0,0 +1,37 @@
+using System.Text.Json;
+
+namespace OnlineOrder.Model
+{
+    public class Product
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public decimal UnitPrice { get; set; }
+
+        public decimal Weight { get; set; }
+
+        //public Supplier Supplier { get; set; }
+        public ProductCategory Category { get; set; }
+
+        public override string ToString()
+        {
+            return  JsonSerializer.Serialize(this);
+        }
+
+        public void Edit(Product product)
+        {
+            if (product == null) return;
+            Name = product.Name;    
+            Description = product.Description;
+            UnitPrice = product.UnitPrice;
+            Weight = product.Weight;
+               
+            
+        }
+
+    }
+}
