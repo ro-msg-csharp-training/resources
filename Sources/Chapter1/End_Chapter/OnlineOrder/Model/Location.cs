28e1facba3a445ff77612a1c194960f1cfd85463
cosminbuda <cosminbuda@hotmail.com>
2022-05-20 09:59:04 +0300

    end of ch1 (#8)
    * end of ch1
    
    * spelling check

--- Sources/Chapter1/End_Chapter/OnlineOrder/Model/Location.cs
+++ Sources/Chapter1/End_Chapter/OnlineOrder/Model/Location.cs
@@ -1,4 +1,6 @@
-﻿namespace OnlineOrder.Model
+﻿using System.Text.Json;
+
+namespace OnlineOrder.Model
 {
     public class Location
     {
@@ -8,5 +10,11 @@
         public string AddressCity { get; set; }
         public string AddressCounty { get; set; }
         public string AddressStreet { get; set; }
+
+
+        public override string ToString()
+        {
+            return JsonSerializer.Serialize(this);
+        }
     }
 }
