1bca0e57b194a01ac67f719007387362a2a4ab9f
cosminbuda <cosminbuda@hotmail.com>
2022-08-01 16:04:27 +0300

    add product route

---
+++ Sources/cos/EF/OnlineOrder/Program.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using OnlineOrder.Data;
+using OnlineOrder.Controllers;
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<OnlineOrderContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("OnlineOrderContext") ?? throw new InvalidOperationException("Connection string 'OnlineOrderContext' not found.")));
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.MapProductEndpoints();
+
+app.Run();
